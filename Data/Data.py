3b42b4954bea2883266627c1e9b10337ee05f22d
WenqiAngieWu <wuwenqi_7@163.com>
2020-04-21 15:27:12 -0400

    Update Data.py

--- Data/Data.py
+++ Data/Data.py
@@ -4,18 +4,17 @@ Fetch data
 """
 
 
-
+from ftplib import FTP
 import pandas as pd
 import bs4 as bs
 import datetime as dt
-#import os
-#import pandas_datareader.data as web
-import pickle
+#import pickle
 import requests
 import pandas_datareader as pdr
 from pandas_datareader._utils import RemoteDataError
 import winsound
 
+#### get symbols of stocks on NASDAQ & NYSE from nasdaqtrader.com ####
 def save_sp500_tickers():
     resp = requests.get('http://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     soup = bs.BeautifulSoup(resp.text, 'lxml')
@@ -24,113 +23,137 @@ def save_sp500_tickers():
     for row in table.findAll('tr')[1:]:
         ticker = row.findAll('td')[0].text
         tickers.append(ticker)
-    with open("sp500tickers.pickle", "wb") as f:
-        pickle.dump(tickers, f)
-    return tickers
+        
+#    with open("sp500tickers.pickle", "wb") as f:
+#        pickle.dump(tickers, f)
+    
+    # write to txt
+    with open('SP500_symbols.txt', 'w') as f:
+        for item in tickers:
+            f.write("%s" % item)
+    
+    sp500_info = pd.read_csv('SP500_symbols.txt', '\n', header = None)
+    
+    # write to csv
+    sp500_info.to_csv('SP500_symbols.csv', index = False, header = ['Symbol'])
 
 
-tickers = save_sp500_tickers()
+#### get symbols of stocks on NASDAQ & NYSE from nasdaqtrader.com ####
+def save_nasdaq_nyse_tickers():
+    
+    directory = 'symboldirectory'
+    filenames = ('otherlisted.txt', 'nasdaqlisted.txt')
+    
+    ftp = FTP('ftp.nasdaqtrader.com')
+    ftp.login()
+    ftp.cwd(directory)
+    
+    for item in filenames:
+        ftp.retrbinary('RETR {0}'.format(item), open(item, 'wb').write)
+        
+    ftp.quit()
+    
+    
+    #### reader symbols, export to csv ####
+    # delete last row: empty row
+    nasdaq_exchange_info = pd.read_csv('nasdaqlisted.txt', '|').iloc[:-1]
+    other_exchange_info = pd.read_csv('otherlisted.txt', '|').iloc[:-1]
+    
+    # to csv
+    nasdaq_exchange_info['Symbol'].to_csv('NASDAQ_symbols.csv', index = False, header = ['Symbol'])
+    other_exchange_info['ACT Symbol'].to_csv('NYSE_symbols.csv', index = False, header = ['Symbol'])
 
-with open('sp500_symbols.txt', 'w') as f:
-    for item in tickers:
-        f.write("%s" % item)
+
+
+    
+
+#### get data according to tickers ####
+def get_data(name, data_type, start, end):
+    
+    csvName = name + '_symbols.csv'
+    names = pd.read_csv(csvName)
+    tickers = names['Symbol'].values.tolist()
+    
+    count = 0
+    while count < len(tickers):
+        if not isinstance(tickers[count], str):
+            tickers.remove(tickers[count])
+        if tickers[count].find('.') == -1:
+            count += 1
+        else:
+            tickers.remove(tickers[count])
+    
+    print("Extracting Data...")
+    
+    
+    df = pdr.DataReader(tickers[0], 'yahoo', start, end)[data_type]
+    print(tickers[0])
+    
+    
+    # try extracting data from Yahoo, throw exception if no data
+    count = 1
+    while count < len(tickers):
+        try:
+            df = pd.concat([df, pdr.DataReader(tickers[count], 'yahoo', start, end)[data_type]], axis=1)
+            print(tickers[count])
+            count += 1
+        except KeyError:
+            print("No information for ticker", tickers[count])
+            tickers.remove(tickers[count])
+            continue
+        except RemoteDataError:
+            print("No information for ticker", tickers[count])
+            tickers.remove(tickers[count])
+            continue
+    
+#    frequency = 500  # Set Frequency To 2500 Hertz
+#    duration = 800 # Set Duration To 1000 ms == 1 second
+#    winsound.Beep(frequency, duration)
+    
+    outputName = name + '_AdjCloseData.csv'
+    df.columns = tickers
+    df.to_csv(outputName)
 
 
 
 
-##########################
-##########################
-##########################
-# start date, end date, data type: High/Low/...
+
+#### get the market index ####
+def get_index(name, data_type, start, end):
+    if name == 'SP500': ticker = '^GSPC'
+    elif name == 'TSX': ticker = '^GSPTSE'
+    elif name == 'NASDAQ': ticker = '^IXIC'
+    elif name == 'NYSE': ticker = '^NYA'
+    
+    
+    df = pdr.DataReader(ticker, 'yahoo', start, end)[data_type] 
+    df = pd.DataFrame(df)
+    df.columns = [name]
+    df.index.names = ['Date']   
+    outputName = name + '.csv'
+    df.to_csv(outputName) 
+  
+####################################################
+####################################################
+
+
+
+save_sp500_tickers()
+save_nasdaq_nyse_tickers()
+
 start = dt.datetime(2006, 1, 1)
 end = dt.datetime(2020, 2, 1)
 data_type = 'Adj Close'
 
-##########################
-# SP500 Index Data
-df = pdr.DataReader('^GSPC', 'yahoo', start, end)[data_type] 
-df = pd.DataFrame(df)
-df.columns = ['SP500']
-df.index.names = ['Date']    
-df.to_csv("SP500.csv") 
-  
-##########################
-
-names = pd.read_csv('sp500_symbols.csv')
-tickers = names['Symbol'].values.tolist()
-
-count = 0
-while count < len(tickers):
-    if not isinstance(tickers[count], str):
-        tickers.remove(tickers[count])
-    if tickers[count].find('.') == -1:
-        count += 1
-    else:
-        tickers.remove(tickers[count])
-
-print("Extracting Data...")
 
 
-
-df = pdr.DataReader(tickers[0], 'yahoo', start, end)[data_type]
-print(tickers[0])
-
-
-# try extracting data from Yahoo, throw exception if no data
-count = 1
-while count < len(tickers):
-    try:
-        df = pd.concat([df, pdr.DataReader(tickers[count], 'yahoo', start, end)[data_type]], axis=1)
-        print(tickers[count])
-        count += 1
-    except KeyError:
-        print("No information for ticker", tickers[count])
-        tickers.remove(tickers[count])
-        continue
-    except RemoteDataError:
-        print("No information for ticker", tickers[count])
-        tickers.remove(tickers[count])
-        continue
-
-# filtered symbols which have data available on Yahoo
-appended_symbols = pd.DataFrame(tickers)
+get_data('SP500', data_type, start, end)
+get_data('NASDAQ', data_type, start, end)
+get_data('NYSE', data_type, start, end)
+get_data('TSX', data_type, start, end)
 
 
-# some of these will not work due to the symbol naming system employed by Yahoo Finance
-appended_symbols.to_csv('SP500SymbolsThatHaveData.csv')
-
-
-df.columns = tickers
-df.to_csv("SP500_AdjCloseData.csv")
-frequency = 500  # Set Frequency To 2500 Hertz
-duration = 800 # Set Duration To 1000 ms == 1 second
-winsound.Beep(frequency, duration)
-
-
-#def get_data_from_yahoo(reload_sp500=False):
-#    if reload_sp500:
-#        tickers = save_sp500_tickers()
-#    else:
-#        with open("sp500tickers.pickle", "rb") as f:
-#            tickers = pickle.load(f)
-#            tickers = tickers[1:]
-#    if not os.path.exists('stock_dfs'):
-#        os.makedirs('stock_dfs')
-#
-#    start = dt.datetime(2008, 1, 1)
-#    end = dt.datetime(2019, 1, 1)
-#    
-#    for ticker in tickers:
-#        # just in case your connection breaks, we'd like to save our progress!
-#        if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
-#            df = web.DataReader(ticker, 'yahoo', start, end)
-#            df.reset_index(inplace=True)
-#            df.set_index("Date", inplace=True)
-#            df = df.drop("Symbol", axis=1)
-#            df.to_csv('stock_dfs/{}.csv'.format(ticker))
-#        else:
-#            print('Already have {}'.format(ticker))
-#    
-#
-#
-#get_data_from_yahoo()
+get_index('SP500', data_type, start, end)
+get_index('NASDAQ', data_type, start, end)
+get_index('NYSE', data_type, start, end)
+get_index('TSX', data_type, start, end)
